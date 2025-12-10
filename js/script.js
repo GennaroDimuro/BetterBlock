a21c2ba2df323fb2cbb3e1d532304e88a2faa6ca
GennaroDimuro <dimuge01@luther.edu>
2025-12-10 03:54:59 +0000

    Update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 'use strict';
 
-
+function selectAll() {
+    let checkAll = document.querySelectorAll("#taskList tbody input[type='checkbox']");
+    for (let cell of checkAll) {
+        cell.checked = true;
+        cell.onclick();
+    }
+}
 
 window.onload = function() {
     console.log("La página cargó completamente.");
